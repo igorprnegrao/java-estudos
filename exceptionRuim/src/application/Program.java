bee02375da818865639fb97ab0236d89e6b53246
igorprnegrao <neurocientista@gmail.com>
2024-12-13 18:47:45 -0300

    .

--- exceptionRuim/src/application/Program.java
+++ exceptionRuim/src/application/Program.java
@@ -34,6 +34,27 @@ public class Program {
 			
 			System.out.println();
 			System.out.println("Enter data to update the reservation: ");
+			
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			//estanciei um date ..mostrado a data atual
+			Date now = new Date();
+			//condicional regra de negocio checkin e checkout não deve ser antes da data atual 
+			//e checkout não deve ser antes do checkin
+			if (checkIn.before(now) || checkOut.before(now) ) {
+				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			} else if (!checkOut.after(checkIn)) {
+				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			} else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println(reservation);
+			}
+			
+			
 		}
 		
 		
