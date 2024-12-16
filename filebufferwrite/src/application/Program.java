92cd922c15cdc668789991b10a65632582485637
igorprnegrao <neurocientista@gmail.com>
2024-12-15 21:57:51 -0300

    .

---
+++ filebufferwrite/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good night"};
+		
+		String path = "/Users/igornegrao/Documents/java-estudos/arquivo1/out.txt"; 
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+}
