cc27013778fa8cada032169ed1e5ba79a66424fe
igorprnegrao <neurocientista@gmail.com>
2024-12-16 12:05:49 -0300

    .

--- interface01/src/application/Program.java
+++ interface01/src/application/Program.java
@@ -1,9 +1,38 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			Locale.setDefault(Locale.US);
+			Scanner sc = new Scanner(System.in);
+			DateTimeFormatter dtf =  DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+			
+			System.out.println("Entre com os dados do aluguel");
+			
+			System.out.print("Modelo do carro:");
+			String model = sc.nextLine();
+			
+			System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+			LocalDateTime start = LocalDateTime.parse(sc.nextLine(), dtf);
+			
+			System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+			LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), dtf);
+			
+			CarRental cr = new CarRental(start, finish, new Vehicle(model));
+			
+			
+			
+			sc.close();
 
 	}
 
