773c364aedc12f104299d4ef73b9e16cf2c85add
igorprnegrao <neurocientista@gmail.com>
2024-12-28 01:08:01 -0300

    .

---
+++ jdbc3/src/db/DB.java
@@ -0,0 +1,75 @@
+package db;
+
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	// biblioteca conection jdbc sl
+	private static Connection conn = null;
+
+	// gerar a conexão com o banco de dados
+	public static Connection getConnection() {
+		try {
+			if (conn == null) {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+
+		return conn;
+	}
+
+	// metodo para fechar a conexão com o banco de dados
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+
+		}
+	}
+
+	// metodo propeties que vai carregar o aquivo e vai tratar a exception caso
+	// ocorra
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
