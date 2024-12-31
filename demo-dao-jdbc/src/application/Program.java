bdb948b57db51f0e3c3e7e6384bfffbdda4a6ac6
igorprnegrao <neurocientista@gmail.com>
2024-12-30 23:53:13 -0300

    ....

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "books");
-		
-		Seller seller = new Seller(21, "joao", "joao@gmail.com", new Date(), 2000.0, obj);
-		
+
 		SellerDao sellerDao = DaoFactory.createrSellerDao();
+
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
