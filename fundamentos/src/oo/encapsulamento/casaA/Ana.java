cbeca144b919c734e4e34f6da02e3a87928e6f06
igorprnegrao <neurocientista@gmail.com>
2025-02-28 20:49:11 -0300

    encapsulamento01

---
+++ fundamentos/src/oo/encapsulamento/casaA/Ana.java
@@ -0,0 +1,11 @@
+package oo.encapsulamento.casaA;
+
+public class Ana {
+		
+	@SuppressWarnings("unused")
+		private String segredo = "...";
+		String  facoDentroDeCasa = "..."; // defaut ou pacote
+		protected String formaDefalar = "...";
+		public String todosSabem = "...";
+		
+}
---
+++ fundamentos/src/oo/encapsulamento/casaA/Paulo.java
@@ -0,0 +1,20 @@
+package oo.encapsulamento.casaA;
+
+public class Paulo {
+			
+			Ana esposa = new Ana();
+			
+			void testeAcesso() {	
+				//segredo 
+				//facoDentroDeCasa 
+				//formaDefalar
+				//todosSabem 
+				
+				System.out.println(esposa.segredo);
+				System.out.println(esposa.facoDentroDeCasa);
+				System.out.println(esposa.formaDefalar);
+				System.out.println(esposa.todosSabem);
+			}
+			
+			
+}
