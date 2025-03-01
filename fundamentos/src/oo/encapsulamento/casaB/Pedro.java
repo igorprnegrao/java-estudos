d4d2bba4aceabd42c130d766654ad22ed9f4f224
igorprnegrao <neurocientista@gmail.com>
2025-02-28 21:02:15 -0300

    encapsulamento02

--- fundamentos/src/oo/encapsulamento/casaB/Pedro.java
+++ fundamentos/src/oo/encapsulamento/casaB/Pedro.java
@@ -1,5 +1,26 @@
 package oo.encapsulamento.casaB;
 
-public class Pedro {
+import java.time.temporal.Temporal;
 
+import oo.encapsulamento.casaA.Ana;
+
+public class Pedro extends Ana {
+			
+	
+	void testeAcessos() {
+		
+		//segredo 
+		//facoDentroDeCasa 
+		//formaDefalar
+		//todosSabem 
+		Ana mae = new Ana();
+		
+		System.out.println(mae.segredo); // Pedro não tem acesso ao sefgredo de ana  
+		System.out.println(mae.facoDentroDeCasa); //Pedro não tem acesso ao atributos defaut de ana
+		System.out.println(mae.formaDefalar); //Pedro não tem acesso ao atributo protected de ana
+		System.out.println(mae.todosSabem); //Pedro tem acesso ao atributo public de ana 
+		
+	}
+	
+	
 }
